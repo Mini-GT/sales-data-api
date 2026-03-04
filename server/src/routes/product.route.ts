313fcc0bb7d1d8dd862fe220df151c66c636d314
MiniGT <bendanillo539@gmail.com>
2026-03-04 13:05:42 +0800

    feat: add authentication middleware to product and sale routes

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -1,12 +1,29 @@
 import productController from "@/controllers/product.controller";
+import authMiddleware, { actionAuth } from "@/middlewares/auth.middleware";
 import type { FastifyInstance } from "fastify";
 
 export default async function productRoutes(fastify: FastifyInstance) {
   fastify.get("/", productController.getAllProductHandler);
-  fastify.post("/register", productController.registerHandler);
+  fastify.post(
+    "/register",
+    { preHandler: [authMiddleware, actionAuth] },
+    productController.registerHandler,
+  );
 
   // must bind the controller due to Fastify having issues when handling class method
-  fastify.patch("/update/:productId", productController.updateHandler.bind(productController));
-  fastify.delete("/delete/:productId", productController.deleteHandler.bind(productController));
-  fastify.get("/:productId", productController.getProductHandler.bind(productController));
+  fastify.patch(
+    "/update/:productId",
+    { preHandler: [authMiddleware, actionAuth] },
+    productController.updateHandler.bind(productController),
+  );
+  fastify.delete(
+    "/delete/:productId",
+    { preHandler: [authMiddleware, actionAuth] },
+    productController.deleteHandler.bind(productController),
+  );
+  fastify.get(
+    "/:productId",
+    { preHandler: [authMiddleware, actionAuth] },
+    productController.getProductHandler.bind(productController),
+  );
 }
--- server/src/routes/sale.route.ts
+++ server/src/routes/sale.route.ts
@@ -1,9 +1,19 @@
 import saleController from "@/controllers/sale.controller";
+import authMiddleware, { actionAuth } from "@/middlewares/auth.middleware";
 import type { FastifyInstance } from "fastify";
 
 export default async function saleRoutes(fastify: FastifyInstance) {
   fastify.get("/", saleController.getMonthlySalesHandler.bind(saleController));
   fastify.post("/", saleController.createSaleHandler.bind(saleController));
-  fastify.get("/:saleId", saleController.getSaleHandler.bind(saleController));
-  fastify.delete("/delete/:saleId", saleController.deleteSaleHandler.bind(saleController));
+
+  fastify.get(
+    "/:saleId",
+    { preHandler: [authMiddleware, actionAuth] },
+    saleController.getSaleHandler.bind(saleController),
+  );
+  fastify.delete(
+    "/delete/:saleId",
+    { preHandler: [authMiddleware, actionAuth] },
+    saleController.deleteSaleHandler.bind(saleController),
+  );
 }
